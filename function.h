8bbb744de3c42e4ba47c2f57b21f87d505e4e645
DanielS005 <71259569+DanielS005@users.noreply.github.com>
2020-10-21 22:11:18 +0400

    Add files via upload

---
+++ function.h
@@ -0,0 +1,2 @@
+void function(const float a, const float xf, const float xd, const float xe);
+void function(const float a, const float x[]);
---
+++ objects.cpp
@@ -0,0 +1,4 @@
+#include "objects.h"
+const float a = 2.0f, xf = 4.2f, xd = 0.6f, xe = 1.2f;
+const float x[] = { 1.6f, 1.32f, 1.47f, 1.65f, 1.93f };
+//float x[5] = { 1.6f, 1.32f, 1.47f, 1.65f, 1.93f };

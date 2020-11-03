5552ebbd41972e01cab0b6877144fedef82f1820
alekseevgang <72067721+alekseevgang@users.noreply.github.com>
2020-11-03 20:28:11 +0300

    Задание от 20 числа

---
+++ 5main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include "function.h"
+#include "objects.h"
+using namespace std; 
+
+
+int main() 
+{
+  float _array_x[5] = {0.08, 0.26, 0.35, 0.41, 0.53};
+  int n = sizeof(_array_x)/sizeof(_array_x[0]);
+   int result = funk( a, b, x);
+    std::cout << "\n " << std::endl;
+    int result2 = funk( a, b, _array_x,n);
+    std::cout << "\n " << std::endl;
+  return 0; 
+}
---
+++ 6function.h
@@ -0,0 +1,2 @@
+float lll(float a, float b, float x);
+float lll(float a,float b, float _array_x[],int n);

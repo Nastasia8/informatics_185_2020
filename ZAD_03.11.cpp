3cbc1b8cc3069b4974b8c5f1a7e9c7be1ec5e870
Anastasya2408 <EAA201@yandex.ru>
2020-11-03 20:17:12 +0300

    ZAD

--- ZAD_03.11.cpp
+++ ZAD_03.11.cpp
@@ -1,10 +1,16 @@
+
 #include <iostream>
 #include <string>
 using namespace std;
+enum CarState{
+    old,
+    new_c
+};
 class Car{
     string brand;
     string model;
     int year;
+    CarState State;
     static int k;
     public:
     Car()
@@ -12,15 +18,25 @@ class Car{
         this->brand="opel";
         this->model="sdsds23";
         this->year=2020;
+        this->State=CarState::new_c;
+        k++;
     }
-    Car(string brand, string model, int year)
+    Car(string brand, string model, int year, CarState State)
     {this->brand=brand;
     this->model=model;
     this->year=9999;
     this->SetYear(year);
+    this->State=State;
     k++;
     }
-
+     void SetState(CarState State)
+     {
+         this->State=State;
+     }
+     CarState GetState()
+     {
+         return State;
+     }
     int GetYear()
     {return year;
     }
@@ -33,14 +49,26 @@ class Car{
     }
     void Show()
     {
-        cout<<"brand"<<brand<<"\t model:"<<model<<"\t year"<<year<<endl;
+        cout<<"brand"<<brand<<"\t model:"<<model<<"\t year"<<year<<"\t State"<<State<<endl;
     }
 };
 int Car::k=0;
 int main()
 { 
-    Car  car("bmw","sds1", -65);
+    Car  car("bmw","sds1", -65, CarState::new_c);
+    Car car1;
+     Car  car2("bmw","wewewe", 1996,CarState::old );
+    car1.Show();
+    car2.Show();
     car.Show();
-    cout<<"kol-vo"<<Car::GetAmong<<endl;
+    cout<<"kol-vo"<<car.GetAmong()<<endl;
+    switch(car2.GetState())
+    {
+        case CarState::new_c:
+        cout<<"The car is new"<<endl; 
+        break;
+         case CarState::old:
+        cout<<"The car is old"<<endl; 
+        break;  }
     return 0;
 }

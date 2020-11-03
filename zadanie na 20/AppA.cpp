23f7c0ba24a9bc749dccad80fcba4e472459ba86
zipliZ <71321221+zipliZ@users.noreply.github.com>
2020-11-03 18:21:57 +0300

    20.10

---
+++ zadanie na 20/AppA.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include "function.h"
+#include "objects.h"
+
+using namespace std;
+
+ int main (){
+     task (a,b);
+
+     return 0;
+
+
+ }
---
+++ zadanie na 20/objects.cpp
@@ -0,0 +1,5 @@
+#include "objects.h"
+
+const float  a=2.5f;
+const float  b=4.6f;
+const float arr[] = {1.2,1.28,1.36,1.46,2.35};

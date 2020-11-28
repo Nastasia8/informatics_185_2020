529d77faa3042b7f1fe8d904a39a9373b77905f9
CruzedV <mr.dobrikv@mail.ru>
2020-11-28 14:41:32 +0300

    Changed year system to month system

--- FinalPracicalExercise/CarTax.cpp
+++ FinalPracicalExercise/CarTax.cpp
@@ -15,6 +15,7 @@ public:
     virtual void Show() = 0;
     virtual void CalculateTax() = 0;
 //Variables
+    //age in month!
     int age;
     //cost in millions!
     float cost;
@@ -50,15 +51,15 @@ public:
         else if (250 < power){ hpCost = 15; }
     //Set multiplier
         //between 3 and 5 millions
-        if ((3 < cost && cost <= 5) && (2 < age && age <= 3 )){ multiplier = 1.1; }
-        else if ((3 < cost && cost <= 5) && (1 <= age && age <= 2 )){ multiplier = 1.3; }
-        else if ((3 < cost && cost <= 5) && (1 > age)){ multiplier = 1.5; }
+        if ((3 < cost && cost <= 5) && (24 < age && age <= 36 )){ multiplier = 1.1; }
+        else if ((3 < cost && cost <= 5) && (12 <= age && age <= 24 )){ multiplier = 1.3; }
+        else if ((3 < cost && cost <= 5) && (12 > age)){ multiplier = 1.5; }
         //between 5 and 10 millions
-        else if (5 < cost && cost <= 10 && age < 5){ multiplier = 2; }
+        else if (5 < cost && cost <= 10 && age < 60){ multiplier = 2; }
         //between 10 and 15 millions
-        else if ((10 < cost && cost <= 15) && age < 10){ multiplier = 3; }
+        else if ((10 < cost && cost <= 15) && age < 120){ multiplier = 3; }
         // more than 15 millions
-        else if (cost > 15 && age < 20){ multiplier = 3; }
+        else if (cost > 15 && age < 240){ multiplier = 3; }
     //Calculate
         tax = multiplier * power * hpCost;
         cout << "Multipier: " << multiplier << " power: " << power << " hpCost: " << hpCost << endl;

15c6e27db379e61f0bd80ede71da2f1cdb718f27
zipliZ <71321221+zipliZ@users.noreply.github.com>
2020-11-28 05:19:00 +0300

    Разбил на файлы задание с фильмами

--- 10.11/second task/film.cpp
+++ 10.11/second task/film.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
-
+#include "film.h"
 using namespace std;
-
-class Film
-{
-private:
-    string name;
-    int year;
-    int rating[3];
-public:
-    float averageRaiting;
-    void set ();
-    float averageRate ();
-    void getBest ();
-};
 
 void Film::set ()
 {
@@ -51,41 +38,4 @@ float Film::averageRate (){
 
 void Film::getBest (){
     cout << "Best film is " << name << ". Its raiting: " <<averageRaiting<< endl ;
-}
-
-int main () {
-    
-    
-
-    Film film1;
-    film1.set();
-    film1.averageRate();
-   
-    
-
-    Film film2;
-    film2.set();
-    film2.averageRate();
-    
-    
-    if (film1.averageRaiting > film2.averageRaiting)
-    {
-       
-       film1.getBest ();
-    }
-    else 
-    {
-        if (film1.averageRaiting==film2.averageRaiting)
-       {
-          film1.getBest ();
-          cout <<  "and ";
-          film2.getBest ();
-
-       }
-       else 
-       film2.getBest ();
-    }
-    
-    return 0;
-
 }

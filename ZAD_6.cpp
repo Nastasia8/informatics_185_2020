b19c00c2cb22e1fe79a2f27c19f51ff0774bcf89
Anastasya2408 <71322369+Anastasya2408@users.noreply.github.com>
2020-12-09 10:12:12 +0300

    Add files via upload

--- ZAD_6.cpp
+++ ZAD_6.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
 using namespace std;
-void chetdiapazon( int nach, int kon)
-{
-int sum=0;
-while (nach <= kon)
-{
-if(nach % 2 ==0)
-{
-sum=sum+nach;
-}
-cout <<nach<< endl;
-nach++;
-}
-cout<<"summ chet "<< sum<<endl;
-
-}
 int main()
 {
-int nach;
-cout<<"Enter start"<<endl;
-cin>>nach;
-
-int kon;
-cout<<"Enter finish"<< endl;
-cin>> kon;
-chetdiapazon( nach, kon);
+int value = 5;
+int *ptr = &value;
+int b=*ptr+20;
+cout « b « endl;
+return 0;
 }

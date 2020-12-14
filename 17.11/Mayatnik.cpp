6da42ab1fe9d1ec01323d74904a74b22c816eb15
zipliZ <71321221+zipliZ@users.noreply.github.com>
2020-12-14 09:06:20 +0300

    updated

--- 17.11/Mayatnik.cpp
+++ 17.11/Mayatnik.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 class Mayatnik
 {
-private:
-    
-public:
 
-    virtual void nyu () = 0; // nyu = частота
+public:
+    const float Pi = 3.141592;
+    const float g = 9.8;
+    virtual void Frequency () = 0; 
     virtual void T () = 0;
     virtual void ShowType ()=0;
 };
@@ -17,9 +17,6 @@ public:
 class Fiz_Mayatnik : public Mayatnik
 {
 private:
-    string type;
-    const float Pi = 3.141592;
-    const float g = 9.8;
     float m,a,I;
     
 public:
@@ -34,7 +31,7 @@ public:
     void T () override {
         cout << "T = "<< (2*Pi*sqrt(I/(m*g*a)))<< endl;
     }
-    void nyu () override {
+    void Frequency () override {
         cout << "Frequency = " << sqrt((m*g*a)/I)<< endl; 
     }
 };
@@ -42,8 +39,6 @@ public:
 class Math_Mayatnik : public Mayatnik
 {
 private:
-    const float Pi = 3.141592;
-    const float g = 9.8;
     float L;
 public:
     Math_Mayatnik(float L){
@@ -55,7 +50,7 @@ public:
     void T () override {
         cout<< "T= " << (2*Pi*sqrt(L/g))<<endl;
     }
-    void nyu () override {
+    void Frequency () override {
         cout << "Frequency= " << (1/(2*Pi)*sqrt(g/L))<< endl;
     }
 };
@@ -65,7 +60,6 @@ public:
 class Pruginniy_Mayatnik : public Mayatnik
 {
 private:
-    const float Pi = 3.141592;
     float m,k;
 public:
     Pruginniy_Mayatnik(float m, float k){
@@ -78,7 +72,7 @@ public:
     void T () override {
         cout<< "T= " << (2*Pi*sqrt(m/k))<<endl;
     }
-    void nyu () override {
+    void Frequency () override {
         cout << "Frequency= " << (1/(2*Pi)*sqrt(k/m))<< endl;
     }
 };
@@ -89,15 +83,15 @@ int main () {
     Math_Mayatnik first  (5);
     first.ShowType();
     first.T();
-    first.nyu();
+    first.Frequency();
     Pruginniy_Mayatnik second  (10,2);
     second.ShowType();
     second.T();
-    second.nyu();
+    second.Frequency();
     Fiz_Mayatnik Third  (5,4,4);
     Third.ShowType();
     Third.T();
-    Third.nyu();
+    Third.Frequency();
     return 0;
 }
 

a5a6d4994ab352f43712ef91fbcb4fad5075c563
anna165 <71322376+anna165@users.noreply.github.com>
2020-12-22 21:51:40 +0300

    Add files via upload
    В дополнительном я добавила матрицу но он у меня писал ошибки и я так и не поняла что не так и не добавила ее сюда

---
+++ 1.py
@@ -0,0 +1,3 @@
+stroc ="The tiger once ranged widely from the Eastern Anatolia Region in the west to the Amur River basin, and in the south from the foothills of the Himalayas to Bali in the Sunda islands."
+print(stroc.count('a'))
+print(stroc.count('s'))

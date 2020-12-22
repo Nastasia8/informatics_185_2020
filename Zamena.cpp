e0e68a0a6db00524a2c67f5cdec1277a35a6d350
NarutoUzumaki <71786909+Juuzoo@users.noreply.github.com>
2020-12-22 22:08:28 +0300

    Add files via upload

---
+++ Zamena.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Zamena(int x, int y)
+
+{
+    int z;
+    z = x;
+    x = y;
+    y = x;
+}
+
+int main()
+{
+    int x, y;
+    cout << "Введите x: ";
+    cin >> x;
+    cout << "Введите y: ";
+    cin >> y;
+    Zamena;
+    cout << "Параметры после обмена местами:" << endl;
+    cout << " x = " << x << endl;
+    cout << " y = " << y << endl;
+
+    return 0;
+}

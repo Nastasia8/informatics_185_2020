30cca51dc74e034d073a075e31e022189610155a
hvlll <hvlll@users.noreply.github.com>
2020-12-23 10:46:45 +0300

    Eksperementy s zmeyukoj

---
+++ Eksperementy s zmeyukoj/classes.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, age, name, breed, color):
+        self.age = age
+        self.name = name
+        self.breed = breed
+        self.color = color
+
+class Auto:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model

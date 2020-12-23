5460f8302f3c574257061e1258a60ca9f50faf13
NarutoUzumaki <71786909+Juuzoo@users.noreply.github.com>
2020-12-23 12:32:57 +0300

    Add files via upload

---
+++ ункции.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include "objects.h"
+#include "function.h"
+
+
+int main()
+{
+    function(a, b, i, in, ip, arr);
+    return 0;
+}
+
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include "objects.h"
+#include "function.h"
+int main()
+{
+    function(a, b, xn, xk, xp);
+    return 0;
+}

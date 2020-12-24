efec5d35f5b9808471c9fb2043f1b4dcb7b7c2b8
svvan45 <71321364+svvan45@users.noreply.github.com>
2020-12-24 12:31:52 +0300

    переделал, сделал классы

--- Задание с рисованием фигур/figura.cpp
+++ Задание с рисованием фигур/figura.cpp
@@ -2,99 +2,145 @@
 #include <string>
 using namespace std;
 
-void Prym_Tr1(int k){
-    char arr[k][k];
-    for (int i= 0; i < k; i++)
-        for (int j = 0; j < k; j++)
-            arr[i][j] = ' ';
-    for (int i = 0; i < k; i++){
-        arr[i][i] = '*';
-        arr[i][0] = '*';
-        arr[k-1][i] = '*';
-         for (int j = 0; j < k; j++){
-             if (arr[i][j] != '*'){
-                 arr[i][j] = ' ';
-             }
-         }
+class Figura{
+    protected:
+    int k, h;
+    public:
+    Figura(int k){
+        this-> k = k;
     }
-    for (int i = 0; i < k; i++){
-        for (int j = 0; j < k; j++){
-            cout << arr[i][j] << " ";
+    Figura(int k, int h){
+        this-> k = k;
+        this-> h = h;
+    }
+};
+class Treygolnik : public Figura{
+public:
+    Treygolnik(int k) : Figura(k){}
+    void Prym_Tr1(){
+        cout << "Prymoygolniy treygolnik V1: " << endl;
+        char arr[k][k];
+        for (int i= 0; i < k; i++)
+            for (int j = 0; j < k; j++)
+                arr[i][j] = ' ';
+        for (int i = 0; i < k; i++){
+            arr[i][i] = '*';
+            arr[i][0] = '*';
+            arr[k-1][i] = '*';
+            for (int j = 0; j < k; j++){
+                if (arr[i][j] != '*'){
+                    arr[i][j] = ' ';
+                }
+            }
         }
-        cout << endl;
-    }
-    
-}
-void Prym_Tr2(int k){
-    char arr[k][k];
-    for (int i= 0; i < k; i++)
-        for (int j = 0; j < k; j++)
-            arr[i][j] = ' ';
-    for (int i = 0; i < k; i++){
-        arr[i][i] = '*';
-        arr[i][k-1] = '*';
-        arr[0][i] = '*';
-         for (int j = 0; j < k; j++){
-             if (arr[i][j] != '*'){
-                 arr[i][j] = ' ';
-             }
-         }
-    }
-    for (int i = 0; i < k; i++){
-        for (int j = 0; j < k; j++){
-            cout << arr[i][j] << " ";
+        for (int i = 0; i < k; i++){
+            for (int j = 0; j < k; j++){
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
         }
-        cout << endl;
+        
     }
-}
-void Ravnobed_tr(int k){
-    int h = 1;
-    for (int i=1; i < k; i++){
-        h +=2;
-    }
-     char arr[k][h];
-     for (int i= 0; i < k; i++)
-        for (int j = 0; j < h; j++)
-            arr[i][j] = ' ';
-     int c = h/2;
-    for (int i=0; i < c+1; i++){
-        arr[i][c-i] = '*';
-        arr[i][c+i] = '*';
-        arr[k-1][i] = '*';
-        arr[k-1][c+i] = '*';
-    }
-   for (int i = 0; i < k; i++){
-        for (int j = 0; j < h; j++){
-            cout << arr[i][j] << " ";
+    void Prym_Tr2(){
+        char arr[k][k];
+        cout << "Prymoygolniy treygolnik V2: " << endl;
+        for (int i= 0; i < k; i++)
+            for (int j = 0; j < k; j++)
+                arr[i][j] = ' ';
+        for (int i = 0; i < k; i++){
+            arr[i][i] = '*';
+            arr[i][k-1] = '*';
+            arr[0][i] = '*';
+            for (int j = 0; j < k; j++){
+                if (arr[i][j] != '*'){
+                    arr[i][j] = ' ';
+                }
+            }
         }
-        cout << endl;
+        for (int i = 0; i < k; i++){
+            for (int j = 0; j < k; j++){
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
     }
+    void Ravnobed_tr(){
+        cout << "Ravnobedrenniy treygolnik: " << endl;
+        int h = 1;
+        for (int i=1; i < k; i++){
+            h +=2;
+        }
+        char arr[k][h];
+        for (int i= 0; i < k; i++)
+            for (int j = 0; j < h; j++)
+                arr[i][j] = ' ';
+        int c = h/2;
+        for (int i=0; i < c+1; i++){
+            arr[i][c-i] = '*';
+            arr[i][c+i] = '*';
+            arr[k-1][i] = '*';
+            arr[k-1][c+i] = '*';
+        }
+    for (int i = 0; i < k; i++){
+            for (int j = 0; j < h; j++){
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
 }
-void prymoyg(int k, int l){
-   char arr1[k][l];
+};
+class Prymoygolnik : public Figura{
+public:
+    Prymoygolnik(int k): Figura(k){}
+    Prymoygolnik(int k, int h): Figura(k, h){}
+void prymoyg(){
+   char arr1[k][h];
+    cout << "Pryamoygolnik: " << endl;
    for (int i= 0; i < k; i++)
-    for (int j = 0; j < l; j++)
+    for (int j = 0; j < h; j++)
         arr1[i][j] = ' ';
 for (int i = 0; i < k; i++){
     arr1[i][0] = '*';
-    arr1[i][l-1] = '*';
-    for(int j = 0; j < l; j++){
+    arr1[i][h-1] = '*';
+    for(int j = 0; j < h; j++){
         arr1[0][j] = '*';
         arr1[k-1][j] = '*';
         }
     }
     for (int i = 0; i < k; i++){
-        for (int j = 0; j < l; j++){
+        for (int j = 0; j < h; j++){
             cout << arr1[i][j] << "  ";
         }
         cout << endl;
     }
 }
-void romb(int k){
+void kvadrat(){
+   char arr1[k][k];
+    cout << "Kvadrat: " << endl;
+   for (int i= 0; i < k; i++)
+    for (int j = 0; j < k; j++)
+        arr1[i][j] = ' ';
+for (int i = 0; i < k; i++){
+    arr1[i][0] = '*';
+    arr1[i][k-1] = '*';
+    for(int j = 0; j < k; j++){
+        arr1[0][j] = '*';
+        arr1[k-1][j] = '*';
+        }
+    }
+    for (int i = 0; i < k; i++){
+        for (int j = 0; j < k; j++){
+            cout << arr1[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+void romb(){
     if (k % 2 == 0){
         k += 1;
     }
     char arr[k][k];
+    cout << "Romb: " << endl;
    for (int i= 0; i < k; i++)
         for (int j = 0; j < k; j++)
             arr[i][j] = ' ';    
@@ -115,11 +161,16 @@ void romb(int k){
         cout << endl;
     }
 }
-void smile1(int k){
+};
+class Smile : public Figura{
+public:
+    Smile(int k): Figura(k){}
+void smile1(){
         if (k % 2 == 0){
         k += 1;
     }
     char arr[k][k];
+    cout << "Smile:  " << endl;
    for (int i= 0; i < k; i++)
         for (int j = 0; j < k; j++)
             arr[i][j] = ' ';    
@@ -147,21 +198,18 @@ for (int i = 0; i < k; i++){
         cout << endl;   
 }
 }
+};
 
 int main(){
-    cout << "Prymoygolniy treygolnik V1: " << endl;
-    Prym_Tr2(8);
-    cout << "Prymoygolniy treygolnik V2: " << endl;
-    Prym_Tr1(10);
-    cout << "Ravnobedrenniy treygolnik: " << endl;
-    Ravnobed_tr(5);
-    cout << "Pryamoygolnik: " << endl;
-    prymoyg(5, 8);
-    cout << "Kvadrat:  " << endl;
-    prymoyg(5, 5);
-    cout << "Romb: " << endl;
-    romb(8);
-    cout << "Smile:  " << endl;
-    smile1(8);
-  
+    Treygolnik a(5);
+    a.Prym_Tr1();
+    a.Prym_Tr2();
+    a.Ravnobed_tr();
+    Prymoygolnik b(8, 5);
+    b.prymoyg();
+    Prymoygolnik c(6);
+    c.kvadrat();
+    c.romb();
+    Smile d(5);
+    d.smile1();
 }

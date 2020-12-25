88c845a3004de3bdf7ef9588084942b6bda2cfe6
zirkazo <71320997+zirkazo@users.noreply.github.com>
2020-12-25 08:46:41 +0300

    Delete 4.py

--- 4.py
+++
@@ -1,9 +0,0 @@
-print ("\n\n\nЧетвёртое задание\n")
-P = float (input ("Сумма вклада: "))
-I = float (input ("Ставка за период: "))
-m = float (input ("Количество начислений процентов в год: "))
-n = float (input ("Количество лет: "))
-def calculateCost():
-     S = round((P * (1 + (I / 100) / (m / 12)) ** (m / 12 * n)), 1)
-     print ("Будущая стоимость вклада:", S)
-calculateCost()

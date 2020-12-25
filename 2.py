79cb3f4f57bd34cabfb245c559fef309efe8ff2a
zirkazo <71320997+zirkazo@users.noreply.github.com>
2020-12-25 08:46:17 +0300

    Delete 2.py

--- 2.py
+++
@@ -1,7 +0,0 @@
-print ("\n\n\n2-ое задание\n")
-def mathPendulum():
-    g = 9,8
-    L = float (input("Введите значение L\n"))
-    result = round((2 * math.pi * math.sqrt(L / g)), 2)
-    print (result)
-mathPendulum()
